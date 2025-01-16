5c08f26aee2f0c63c0bef147f6e36d493ea90425
Luísa Franco <89918826+francoluisa@users.noreply.github.com>
2025-01-16 14:32:44 -0300

    Add files via upload

---
+++ 1- Logica de programação mergulhe em programação com JavaScript/desafio/logica-js-projeto_inicial/app.js
@@ -0,0 +1,28 @@
+/* let contador = 1;
+while (contador <=10){
+    alert(`Contador: ${contador}`);
+    contador++;
+}
+
+let contador = 10;
+
+while (contador >= 0) {
+    alert(`Contador ${contador}`);
+    contador--;
+}
+
+
+let numero = prompt("Digite um numero"); 
+
+while (numero >= 0) {
+    console.log(`Número ${numero}`);
+    numero--;
+}
+
+let numero = prompt("Digite um numero"); 
+let contador = 0;
+while (contador <= numero) {
+    console.log(`Número ${contador}`);
+    contador++;
+}
+ */
