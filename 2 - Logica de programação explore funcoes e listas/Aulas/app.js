16662b4a55e8aafd908025e2e63ac5978cc24752
luisa <luisafranconeves@gmail.com>
2025-01-17 16:00:07 -0300

    aula 1 e desafio

--- 2 - Logica de programação explore funcoes e listas/Aulas/app.js
+++ 2 - Logica de programação explore funcoes e listas/Aulas/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute (){
+    console.log('O botão foi clicado');
+}
