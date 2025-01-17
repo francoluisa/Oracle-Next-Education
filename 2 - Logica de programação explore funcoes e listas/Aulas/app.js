c7d320258fb5088b1a3fce5bff2398ae86abd2b8
luisa <luisafranconeves@gmail.com>
2025-01-17 16:52:01 -0300

    2.3

--- 2 - Logica de programação explore funcoes e listas/Aulas/app.js
+++ 2 - Logica de programação explore funcoes e listas/Aulas/app.js
@@ -2,7 +2,10 @@
 titulo.innerHTML = 'Jogo número secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; */
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; 
+*/
+
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -12,7 +15,10 @@ function exibirTextoNaTela(tag, texto){
 exibirTextoNaTela('h1', 'Jogo número secreto')
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 
-
 function verificarChute (){
-    console.log('O botão foi clicado');
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10) + 1;
 }
