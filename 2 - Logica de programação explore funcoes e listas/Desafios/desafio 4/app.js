ec059e6d7de1281c4a9c443b778ed03c1db079a7
luisa <luisafranconeves@gmail.com>
2025-01-22 12:35:34 -0300

    aula 4 e desafios

---
+++ 2 - Logica de programação explore funcoes e listas/Desafios/desafio 4/app.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagemDeProgramação.push('Java', 'Ruby', 'GoLang');
+
+let listaNomes = ['Ana','Bob','Ray'];
+console.log(listaNomes[0]);
+
+let listaNomes2 = ['Lila','Paz','Peter'];
+console.log(listaNomes2[1]);
+
+let listaNomes3 = ['Theo','Jay','Alex'];
+console.log(listaNomes3[2]);
