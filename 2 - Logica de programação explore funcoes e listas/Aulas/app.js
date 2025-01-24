07de31cabeaf99ce65a9afb072509f9550a7a889
luisa <luisafranconeves@gmail.com>
2025-01-24 10:18:04 -0300

    de 1 a 100

--- 2 - Logica de programação explore funcoes e listas/Aulas/app.js
+++ 2 - Logica de programação explore funcoes e listas/Aulas/app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 mensagemInicial();
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto){
 
 function mensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function verificarChute (){
